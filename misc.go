72e3cb6308dba30ba4e8f0aaa508f3c591722b36
Steve Yen <steve.yen@gmail.com>
2014-11-20 09:40:35 -0800

    initial rebalance.go and misc.go files

---
+++ misc.go
@@ -0,0 +1,51 @@
+//  Copyright (c) 2014 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the
+//  License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an "AS
+//  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//  express or implied. See the License for the specific language
+//  governing permissions and limitations under the License.
+
+package blance
+
+// StringsToMap converts an array of strings to an map keyed by
+// strings, so the caller can have faster lookups.
+func StringsToMap(strsArr []string) map[string]bool {
+	if strsArr == nil {
+		return nil
+	}
+	strs := map[string]bool{}
+	for _, str := range strsArr {
+		strs[str] = true
+	}
+	return strs
+}
+
+// StringsRemoveStrings returns a copy of stringArr, but with some
+// strings removed, keeping the same order as stringArr.
+func StringsRemoveStrings(stringArr, removeArr []string) []string {
+	removeMap := StringsToMap(removeArr)
+	rv := make([]string, 0, len(stringArr))
+	for _, s := range stringArr {
+		if !removeMap[s] {
+			rv = append(rv, s)
+		}
+	}
+	return rv
+}
+
+// StringsIntersectStrings returns a brand new array that has the
+// intersection of a and b.
+func StringsIntersectStrings(a, b []string) []string {
+	bMap := StringsToMap(b)
+	rv := make([]string, 0, len(a))
+	for _, s := range a {
+		if bMap[s] {
+			rv = append(rv, s)
+		}
+	}
+	return rv
+}
---
+++ rebalance.go
@@ -0,0 +1,224 @@
+//  Copyright (c) 2014 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the
+//  License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an "AS
+//  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//  express or implied. See the License for the specific language
+//  governing permissions and limitations under the License.
+
+// The blance package provides a simple partition rebalancing
+// algorithm, using a greedy, single-pass, heuristic approach.
+package blance
+
+import (
+	"fmt"
+	"sort"
+)
+
+type PartitionMap struct {
+	Partitions []*Partition
+}
+
+type Partition struct {
+	// Name must be unique within a PartitionMap.Partitions.
+	Name string
+
+	// Key is stateName, value is array of node names.
+	NodesByState map[string][]string
+}
+
+type PartitionModel struct {
+	// Keyed by stateName, like "master", "slave", "dead", etc.
+	States map[string]*PartitionModelState
+}
+
+type PartitionModelState struct {
+	// Priority of zero is the highest.  e.g., "master" Priority
+	// should be < than "slave" Priority.
+	Priority    int
+	Constraints int
+}
+
+func RebalancePartitions(
+	prevMap *PartitionMap,
+	nodesToRemove []string,
+	nodesToAdd []string,
+	model *PartitionModel,
+	// Keyed by same key as the key to partitionModel.States, e.g.,
+	// "master", "slave", "dead", etc.
+	modelStateConstraints map[string]int,
+	partitionWeights map[string]int,
+) *PartitionMap {
+	// Start by filling out nextPartitions as a deep clone of
+	// prevMap.Partitions, but filter out the to-be-removed nodes.
+	nextPartitions := make([]*Partition, 0, len(prevMap.Partitions))
+	for _, prevPartition := range prevMap.Partitions {
+		n := removeNodesFromNodesByState(prevPartition.NodesByState, nodesToRemove)
+		nextPartitions = append(nextPartitions, &Partition{
+			Name:         prevPartition.Name,
+			NodesByState: n,
+		})
+	}
+
+	// Given a PartitionModel state name and its constraints, for
+	// every partition, assign nodes.
+	assignStateToPartitions := func(stateName string, constraints int) {
+		// Sort the partitions to help reach a better assignment.
+		p := &partitionSorter{
+			stateName:         stateName,
+			prevMapPartitions: make(map[string]*Partition),
+			nodesToRemove:     nodesToRemove,
+			nodesToAdd:        nodesToAdd,
+			partitionWeights:  partitionWeights,
+			a:                 append([]*Partition(nil), nextPartitions...),
+		}
+		for _, prevMapPartition := range prevMap.Partitions {
+			p.prevMapPartitions[prevMapPartition.Name] = prevMapPartition
+		}
+		sort.Sort(p)
+
+		// TODO: complete the rest of the algorithm.
+	}
+
+	// Run through the sorted partition states (master, slave, etc)
+	// that have constraints and invoke assignStateToPartitions().
+	pms := &stateNameSorter{
+		m: model,
+		s: make([]string, 0, len(model.States)),
+	}
+	for stateName, _ := range model.States {
+		pms.s = append(pms.s, stateName)
+	}
+	sort.Sort(pms)
+	for _, stateName := range pms.s {
+		constraints, exists := modelStateConstraints[stateName]
+		if !exists {
+			modelState, exists := model.States[stateName]
+			if exists && modelState != nil {
+				constraints = modelState.Constraints
+			}
+		}
+		if constraints > 0 {
+			assignStateToPartitions(stateName, constraints)
+		}
+	}
+
+	return nil
+}
+
+// --------------------------------------------------------
+
+// Returns a copy of nodesByState but with nodes removed.
+// Example, when removeNodes == ["a"] and
+// nodesByState == {"master": ["a"], "slave": ["b"]},
+// then result will be {"master": [], "slave": ["b"]}.
+func removeNodesFromNodesByState(
+	nodesByState map[string][]string,
+	removeNodes []string,
+) map[string][]string {
+	rv := make(map[string][]string)
+	for stateName, nodes := range nodesByState {
+		rv[stateName] = StringsRemoveStrings(nodes, removeNodes)
+	}
+	return rv
+}
+
+// Given a nodesByState, like {"master": ["a"], "slave": ["b", "c"]},
+// this function might return something like ["b", "c", "a"].
+func flattenNodesByState(nodesByState map[string][]string) []string {
+	rv := make([]string, 0)
+	for _, nodes := range nodesByState {
+		rv = append(rv, nodes...)
+	}
+	return rv
+}
+
+// --------------------------------------------------------
+
+// Does ORDER BY m.States[stateName].Priority ASC, stateName ASC".
+type stateNameSorter struct {
+	m *PartitionModel
+	s []string // Mutated array of partition model state names.
+}
+
+func (pms *stateNameSorter) Len() int {
+	return len(pms.s)
+}
+
+func (pms *stateNameSorter) Less(i, j int) bool {
+	return pms.m.States[pms.s[i]].Priority < pms.m.States[pms.s[j]].Priority ||
+		pms.s[i] < pms.s[j]
+}
+
+func (pms *stateNameSorter) Swap(i, j int) {
+	pms.s[i], pms.s[j] = pms.s[j], pms.s[i]
+}
+
+// --------------------------------------------------------
+
+// Does ORDER BY partitions-on-nodes-to-be-removed, then by
+// partitions-who-haven't-been-assigned-anywhere-yet, then by
+// partition-weight, then by partition-name.
+type partitionSorter struct {
+	stateName         string
+	prevMapPartitions map[string]*Partition // Keyed by partition name.
+	nodesToRemove     []string
+	nodesToAdd        []string
+	partitionWeights  map[string]int // Keyed by partition name.
+
+	a []*Partition // Mutated during sort.
+}
+
+func (r *partitionSorter) Len() int {
+	return len(r.a)
+}
+
+func (r *partitionSorter) Less(i, j int) bool {
+	ei := r.Entry(i)
+	ej := r.Entry(j)
+
+	for x := 0; x < len(ei) && x < len(ej); x++ {
+		if ei[x] < ej[x] {
+			return true
+		}
+	}
+
+	return len(ei) < len(ej)
+}
+
+func (r *partitionSorter) Swap(i, j int) {
+	r.a[i], r.a[j] = r.a[j], r.a[i]
+}
+
+func (r *partitionSorter) Entry(i int) []string {
+	partitionName := r.a[i].Name
+
+	// Calculate space padded (for sortability) partition weight.
+	partitionWeight := 1
+	if r.partitionWeights != nil {
+		if w, exists := r.partitionWeights[partitionName]; exists {
+			partitionWeight = w
+		}
+	}
+	partitionWeightStr := fmt.Sprintf("%20d", partitionWeight)
+
+	// First, favor partitions on nodes that are to-be-removed.
+	lastPartition := r.prevMapPartitions[partitionName]
+	lastPartitionNBS := lastPartition.NodesByState[r.stateName]
+	if lastPartitionNBS != nil &&
+		len(StringsIntersectStrings(lastPartitionNBS, r.nodesToRemove)) > 0 {
+		return []string{"0", partitionWeightStr, partitionName}
+	}
+
+	// Then, favor partitions who haven't yet been assigned to any
+	// newly added nodes yet for any state.
+	fnbs := flattenNodesByState(r.a[i].NodesByState)
+	if len(StringsIntersectStrings(fnbs, r.nodesToAdd)) <= 0 {
+		return []string{"1", partitionWeightStr, partitionName}
+	}
+
+	return []string{"2", partitionWeightStr, partitionName}
+}
