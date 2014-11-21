b3d572d9b76109dfc5adfcc78c00b17c7f83ed52
Steve Yen <steve.yen@gmail.com>
2014-11-20 22:28:33 -0800

    more StringsToMap() testing

--- misc_test.go
+++ misc_test.go
@@ -1,6 +1,7 @@
 package blance
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -9,5 +10,13 @@ func TestStringsToMap(t *testing.T) {
 	m := StringsToMap(s)
 	if m == nil || len(m) != 0 {
 		t.Errorf("expected StringsToMap to work on empty array")
+	}
+	m = StringsToMap([]string{"a"})
+	if m == nil || !reflect.DeepEqual(m, map[string]bool{"a": true}) {
+		t.Errorf("expected single string arr to work")
+	}
+	m = StringsToMap([]string{"a", "b", "a"})
+	if m == nil || !reflect.DeepEqual(m, map[string]bool{"a": true, "b": true}) {
+		t.Errorf("expected 3 string arr to work with dupe removal")
 	}
 }
