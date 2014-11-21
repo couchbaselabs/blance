7c36f7e608c7c2c3149ebad5b78d09504a4c0192
Steve Yen <steve.yen@gmail.com>
2014-11-20 22:56:54 -0800

    TestRemoveNodesFromNodesByState()

--- plan_test.go
+++ plan_test.go
@@ -35,3 +35,68 @@ func TestflattenNodesByState(t *testing.T) {
 		}
 	}
 }
+
+func TestRemoveNodesFromNodesByState(t *testing.T) {
+	tests := []struct {
+		nodesByState map[string][]string
+		removeNodes  []string
+		exp          map[string][]string
+	}{
+		{map[string][]string{"master": []string{"a", "b"}},
+			[]string{"a", "b"},
+			map[string][]string{"master": []string{}},
+		},
+		{map[string][]string{"master": []string{"a", "b"}},
+			[]string{"b", "c"},
+			map[string][]string{"master": []string{"a"}},
+		},
+		{map[string][]string{"master": []string{"a", "b"}},
+			[]string{"a", "c"},
+			map[string][]string{"master": []string{"b"}},
+		},
+		{map[string][]string{"master": []string{"a", "b"}},
+			[]string{},
+			map[string][]string{"master": []string{"a", "b"}},
+		},
+		{
+			map[string][]string{
+				"master": []string{"a", "b"},
+				"slave":  []string{"c"},
+			},
+			[]string{},
+			map[string][]string{
+				"master": []string{"a", "b"},
+				"slave":  []string{"c"},
+			},
+		},
+		{
+			map[string][]string{
+				"master": []string{"a", "b"},
+				"slave":  []string{"c"},
+			},
+			[]string{"a"},
+			map[string][]string{
+				"master": []string{"b"},
+				"slave":  []string{"c"},
+			},
+		},
+		{
+			map[string][]string{
+				"master": []string{"a", "b"},
+				"slave":  []string{"c"},
+			},
+			[]string{"a", "c"},
+			map[string][]string{
+				"master": []string{"b"},
+				"slave":  []string{},
+			},
+		},
+	}
+	for i, c := range tests {
+		r := removeNodesFromNodesByState(c.nodesByState, c.removeNodes, nil)
+		if !reflect.DeepEqual(r, c.exp) {
+			t.Errorf("i: %d, nodesByState: %#v, removeNodes: %#v, exp: %#v, got: %#v",
+				i, c.nodesByState, c.removeNodes, c.exp, r)
+		}
+	}
+}
