d1a20746cc35cd7484b14d92157ea1879b47b893
Steve Yen <steve.yen@gmail.com>
2014-11-21 16:12:12 -0800

    test 2 nodes, swap nodes a & b for c & d

--- plan_test.go
+++ plan_test.go
@@ -797,6 +797,57 @@ func TestPlanNextMap(t *testing.T) {
 			},
 			expNumWarnings: 0,
 		},
+		{
+			About: "2 nodes, swap nodes a & b for c & d",
+			PrevMap: PartitionMap{
+				"0": &Partition{
+					Name: "0",
+					NodesByState: map[string][]string{
+						"master": []string{"a"},
+						"slave":  []string{"b"},
+					},
+				},
+				"1": &Partition{
+					Name: "1",
+					NodesByState: map[string][]string{
+						"master": []string{"b"},
+						"slave":  []string{"a"},
+					},
+				},
+			},
+			Nodes:         []string{"a", "b", "c", "d"},
+			NodesToRemove: []string{"a", "b"},
+			NodesToAdd:    []string{"c", "d"},
+			Model: PartitionModel{
+				"master": &PartitionModelState{
+					Priority: 0, Constraints: 1,
+				},
+				"slave": &PartitionModelState{
+					Priority: 1, Constraints: 1,
+				},
+			},
+			ModelStateConstraints: nil,
+			PartitionWeights:      nil,
+			StateStickiness:       nil,
+			NodeWeights:           nil,
+			exp: PartitionMap{
+				"0": &Partition{
+					Name: "0",
+					NodesByState: map[string][]string{
+						"master": []string{"c"},
+						"slave":  []string{"d"},
+					},
+				},
+				"1": &Partition{
+					Name: "1",
+					NodesByState: map[string][]string{
+						"master": []string{"d"},
+						"slave":  []string{"c"},
+					},
+				},
+			},
+			expNumWarnings: 0,
+		},
 	}
 	for i, c := range tests {
 		r, rWarnings := PlanNextMap(
