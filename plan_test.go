359f55f940b46cfecdd0a5ebdd9892701d16b564
Steve Yen <steve.yen@gmail.com>
2014-11-21 16:02:15 -0800

    test 2 nodes remove 3

--- plan_test.go
+++ plan_test.go
@@ -593,6 +593,57 @@ func TestPlanNextMap(t *testing.T) {
 			},
 			expNumWarnings: 4,
 		},
+		{
+			About: "2 nodes, remove 3",
+			PrevMap: PartitionMap{
+				"0": &Partition{
+					Name: "0",
+					NodesByState: map[string][]string{
+						"master": []string{"a"},
+						"slave":  []string{"b"},
+					},
+				},
+				"1": &Partition{
+					Name: "1",
+					NodesByState: map[string][]string{
+						"master": []string{"b"},
+						"slave":  []string{"a"},
+					},
+				},
+			},
+			Nodes:         []string{"a", "b", "c"},
+			NodesToRemove: []string{"c", "b", "a"},
+			NodesToAdd:    []string{},
+			Model: PartitionModel{
+				"master": &PartitionModelState{
+					Priority: 0, Constraints: 1,
+				},
+				"slave": &PartitionModelState{
+					Priority: 1, Constraints: 1,
+				},
+			},
+			ModelStateConstraints: nil,
+			PartitionWeights:      nil,
+			StateStickiness:       nil,
+			NodeWeights:           nil,
+			exp: PartitionMap{
+				"0": &Partition{
+					Name: "0",
+					NodesByState: map[string][]string{
+						"master": []string{},
+						"slave":  []string{},
+					},
+				},
+				"1": &Partition{
+					Name: "1",
+					NodesByState: map[string][]string{
+						"master": []string{},
+						"slave":  []string{},
+					},
+				},
+			},
+			expNumWarnings: 4,
+		},
 	}
 	for i, c := range tests {
 		r, rWarnings := PlanNextMap(
