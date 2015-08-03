92801aed8886ee325a729f03be5474882a9e3354
Steve Yen <steve.yen@gmail.com>
2015-08-02 19:01:40 -0700

    renamed to doneCh to match context and more func types

--- orchestrate.go
+++ orchestrate.go
@@ -11,6 +11,8 @@
 
 package blance
 
+// An Orchestrator instance holds the runtime state during an
+// OrchestrateMoves() operation.
 type Orchestrator struct {
 	label string
 
@@ -23,16 +25,44 @@ type Orchestrator struct {
 	destMap PartitionMap
 	currMap func() (PartitionMap, error)
 
-	assignPartition func(partition string, node string,
-		state string, priority int, fromNode string) error
-	unassignPartition func(partition string, node string,
-		state string, priority int) error
-	partitionState func(partition string, node string) (
-		state string, priority int, pct float32, err error)
+	assignPartition   AssignPartitionFunc
+	unassignPartition UnassignPartitionFunc
+	partitionState    PartitionStateFunc
 
 	progressCh chan OrchestratorProgress
-	controlCh  chan string
+	doneCh     chan struct{}
+
+	tokensSupplyCh  chan int
+	tokensReleaseCh chan int
 }
+
+// AssignPartitionFunc is a callback invoked by OrchestrateMoves()
+// when it wants to asynchronously assign a partition to a node.
+type AssignPartitionFunc func(
+	partition string,
+	node string,
+	state string,
+	insertAt int,
+	fromNode string,
+	fromNodeTakeOver bool) error
+
+// UnassignPartitionFunc is a callback invoked by OrchestrateMoves()
+// when it wants to asynchronously remove a partition from a node.
+type UnassignPartitionFunc func(
+	partition string,
+	node string,
+	state string) error
+
+// UnassignPartitionFunc is a callback invoked by OrchestrateMoves()
+// when it wants to synchronously retrieve information about a
+// partition on a node.
+type PartitionStateFunc func(
+	partition string,
+	node string) (
+	state string,
+	position int,
+	pct float32,
+	err error)
 
 // OrchestratorMoves asynchronously begins reassigning partitions
 // amongst nodes to reach the destMap state, invoking the callback
@@ -45,12 +75,9 @@ func OrchestrateMoves(
 	nodesAll []string,
 	destMap PartitionMap,
 	currMap func() (PartitionMap, error),
-	assignPartition func(partition, node, state string,
-		priority int, fromNode string) error,
-	unassignPartition func(partition, node, state string,
-		priority int) error,
-	partitionState func(partition, node string) (
-		state string, priority int, pct float32, err error),
+	assignPartition AssignPartitionFunc,
+	unassignPartition UnassignPartitionFunc,
+	partitionState PartitionStateFunc,
 ) (*Orchestrator, error) {
 	o := &Orchestrator{
 		label:             label,
@@ -63,7 +90,9 @@ func OrchestrateMoves(
 		unassignPartition: unassignPartition,
 		partitionState:    partitionState,
 		progressCh:        make(chan OrchestratorProgress),
-		controlCh:         make(chan string),
+		doneCh:            make(chan struct{}),
+		tokensSupplyCh:    make(chan int),
+		tokensReleaseCh:   make(chan int),
 	}
 
 	go o.run()
@@ -74,7 +103,7 @@ func OrchestrateMoves(
 // Stop asynchronously requests the orchestrator to stop, where the
 // caller will eventually see a closed progress channel.
 func (o *Orchestrator) Stop() {
-	close(o.controlCh)
+	close(o.doneCh)
 }
 
 // ProgressCh returns a channel that is updated occassionally when the
@@ -116,7 +145,106 @@ type OrchestratorProgress struct {
 }
 
 func (o *Orchestrator) run() {
+	nodesDoneCh := make(chan error)
+
+	for _, node := range o.nodesAll {
+		go func() {
+			nodesDoneCh <- o.runNode(node)
+		}()
+	}
+
+	m := o.options.MaxConcurrentPartitionMovesPerCluster
+	for i := 0; i < m; i++ {
+		// Tokens available to throttle concurrency.  The # of
+		// outstanding tokens might be changed dynamically and can
+		// also be used to synchronize with any optional, external
+		// manager (i.e., ns-server wants cbft to do X number of moves
+		// with M concurrency before forcing a cluster-wide
+		// compaction).
+		o.tokensSupplyCh <- i
+	}
+
+	for range o.nodesAll {
+		<-nodesDoneCh
+	}
+
+	close(o.progressCh)
+}
+
+func (o *Orchestrator) runNode(node string) error {
+	for {
+		select {
+		case _, ok := <-o.doneCh:
+			if !ok {
+				return nil
+			}
+
+		case token, ok := <-o.tokensSupplyCh:
+			if !ok {
+				return nil
+			}
+
+			partition, state, insertAt, fromNode, fromNodeTakeOver, err :=
+				o.calcNextPartitionToAssignToNode(node)
+			if err != nil {
+				o.tokensReleaseCh <- token
+				return err
+			}
+
+			err = o.assignPartition(partition, node, state,
+				insertAt, fromNode, fromNodeTakeOver)
+			if err != nil {
+				o.tokensReleaseCh <- token
+				return err
+			}
+
+			err = o.waitForPartitionNodeState(partition,
+				node, state, insertAt)
+			if err != nil {
+				o.tokensReleaseCh <- token
+				return err
+			}
+
+			if fromNode != "" {
+				err = o.unassignPartition(partition, node, state)
+				if err != nil {
+					o.tokensReleaseCh <- token
+					return err
+				}
+
+				err = o.waitForPartitionNodeState(partition,
+					node, "", -1)
+				if err != nil {
+					o.tokensReleaseCh <- token
+					return err
+				}
+			}
+
+			o.tokensReleaseCh <- token
+		}
+	}
+
+	return nil
+}
+
+func (o *Orchestrator) calcNextPartitionToAssignToNode(node string) (
+	partition string,
+	state string,
+	insertAt int,
+	fromNode string,
+	fromNodeTakeOver bool,
+	err error) {
 	// TODO.
+	return "", "", 0, "", false, nil
+}
+
+func (o *Orchestrator) waitForPartitionNodeState(
+	partition string,
+	node string,
+	state string,
+	position int) error {
+	// TODO.
+	return nil
 }
 
 /*
