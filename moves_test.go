927ad970b9b21517213e3cf6cf7acde33859be54
Steve Yen <steve.yen@gmail.com>
2015-08-05 19:01:39 -0700

    TestFindStateChanges

--- moves_test.go
+++ moves_test.go
@@ -1,8 +1,141 @@
 package blance
 
 import (
+	"reflect"
 	"testing"
 )
+
+func TestFindStateChanges(t *testing.T) {
+	tests := []struct {
+		begStateIdx     int
+		endStateIdx     int
+		state           string
+		states          []string
+		begNodesByState map[string][]string
+		endNodesByState map[string][]string
+		expected        []string
+	}{
+		{0, 0, "master",
+			[]string{"master", "replica"},
+			map[string][]string{
+				"master":  []string{"a"},
+				"replica": []string{"b", "c"},
+			},
+			map[string][]string{
+				"master":  []string{"a"},
+				"replica": []string{"b", "c"},
+			},
+			nil,
+		},
+		{1, 2, "master",
+			[]string{"master", "replica"},
+			map[string][]string{
+				"master":  []string{"a"},
+				"replica": []string{"b", "c"},
+			},
+			map[string][]string{
+				"master":  []string{"a"},
+				"replica": []string{"b", "c"},
+			},
+			nil,
+		},
+		{0, 0, "master",
+			[]string{"master", "replica"},
+			map[string][]string{
+				"master":  []string{},
+				"replica": []string{"a"},
+			},
+			map[string][]string{
+				"master":  []string{"a"},
+				"replica": []string{},
+			},
+			nil,
+		},
+		{1, 2, "master",
+			[]string{"master", "replica"},
+			map[string][]string{
+				"master":  []string{},
+				"replica": []string{"a"},
+			},
+			map[string][]string{
+				"master":  []string{"a"},
+				"replica": []string{},
+			},
+			[]string{"a"},
+		},
+		{0, 1, "replica",
+			[]string{"master", "replica"},
+			map[string][]string{
+				"master":  []string{"a"},
+				"replica": []string{},
+			},
+			map[string][]string{
+				"master":  []string{},
+				"replica": []string{"a"},
+			},
+			[]string{"a"},
+		},
+		{1, 2, "replica",
+			[]string{"master", "replica"},
+			map[string][]string{
+				"master":  []string{"a"},
+				"replica": []string{},
+			},
+			map[string][]string{
+				"master":  []string{},
+				"replica": []string{"a"},
+			},
+			nil,
+		},
+		{1, 2, "replica",
+			[]string{"master", "replica"},
+			map[string][]string{
+				"master":  []string{},
+				"replica": []string{"a"},
+			},
+			map[string][]string{
+				"master":  []string{},
+				"replica": []string{},
+			},
+			nil,
+		},
+		{1, 2, "master",
+			[]string{"master", "replica"},
+			map[string][]string{
+				"master":  []string{"a"},
+				"replica": []string{"b", "c", "d"},
+			},
+			map[string][]string{
+				"master":  []string{"b"},
+				"replica": []string{"a", "c", "d"},
+			},
+			[]string{"b"},
+		},
+		{1, 2, "master",
+			[]string{"master", "replica"},
+			map[string][]string{
+				"master":  []string{"a"},
+				"replica": []string{"b", "c", "d"},
+			},
+			map[string][]string{
+				"master":  []string{"x"},
+				"replica": []string{"a", "c", "d"},
+			},
+			nil,
+		},
+	}
+
+	for i, test := range tests {
+		got := findStateChanges(test.begStateIdx, test.endStateIdx,
+			test.state, test.states,
+			test.begNodesByState,
+			test.endNodesByState)
+		if !reflect.DeepEqual(got, test.expected) {
+			t.Errorf("i: %d, got: %#v, expected: %#v, test: %#v",
+				i, got, test.expected, test)
+		}
+	}
+}
 
 func TestCalcPartitionMoves(t *testing.T) {
 	tests := []struct {
